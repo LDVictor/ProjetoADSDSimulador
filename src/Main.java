363bc8ffabff949b7660952ef7061d80c232e501
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-10-31 15:58:38 -0300

    add simjava to lib

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import eduni.simjava.Sim_entity;
+
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+}
+
+class SimEntity extends Sim_entity {
+
+  public SimEntity(String s) {
+    super(s);
+  }
+
+  public SimEntity(String s, String s1, int i, int i1) {
+    super(s, s1, i, i1);
+  }
 }
