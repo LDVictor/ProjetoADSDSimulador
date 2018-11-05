ff3726cb4078188a60d6908f7bff51412059bdaa
Victor Emanuel Farias da Costa Borges <victor.borges@ccc.ufcg.edu.br>
2018-11-05 20:06:03 -0300

    Finalizando main.

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,48 @@ import eduni.simjava.Sim_entity;
 
 public class Main {
 
+    @SuppressWarnings("unused")
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Sim_system.initialise();
+
+        FichaDeAtendimento fichaDeAtendimento = new FichaDeAtendimento("FichaDeAtendimento", 3);
+
+        BalcaoDeAtendimento balcaoDeAtendimento = new BalcaoDeAtendimento("BalcaoDeAtendimento", 3, 1);
+
+        Triagem triagem = new Triagem("Triagem", 3, 1);
+
+        ClinicoGeral clinicoGeral = new ClinicoGeral("ClinicoGeral", 3, 2);
+
+        Otorrinolaringologista otorrinolaringologista = new Otorrinolaringologista("Otorrinolaringologista", 3, 2);
+
+        Cardiologista cardiologista = new Cardiologista("Cardiologista", 3, 2);
+
+        PostoDeEnfermagem postoDeEnfermagem = new PostoDeEnfermagem("PostoDeEnfermagem", 10, 5);
+
+        AlaDeMedicacao alaDeMedicacao = new AlaDeMedicacao("AlaDeMedicacao", 5);
+
+        //Configuração das portas (Entidade de Origem, Porta de Origem, Entidade de Destino, PortaDestino)
+
+        Sim_system.link_ports("FichaDeAtendimento", "BalcaoDeAtendimento", "BalcaoDeAtendimento", "FichaDeAtendimento");
+
+        Sim_system.link_ports("BalcaoDeAtendimento", "Triagem", "Triagem", "BalcaoDeAtendimento");
+
+        Sim_system.link_ports("Triagem", "ClinicoGeral", "ClinicoGeral", "Triagem");
+        Sim_system.link_ports("Triagem", "Otorrinolaringologista", "Otorrinolaringologista", "Triagem");
+        Sim_system.link_ports("Triagem", "Cardiologista", "Cardiologista", "Triagem");
+
+        Sim_system.link_ports("ClinicoGeral", "PostoDeEnfermagem", "PostoDeEnfermagem", "ClinicoGeral");
+        Sim_system.link_ports("Otorrinolaringologista", "PostoDeEnfermagem", "PostoDeEnfermagem", "Otorrinolaringologista");
+        Sim_system.link_ports("Cardiologista", "PostoDeEnfermagem", "PostoDeEnfermagem", "Cardiologista");
+
+        Sim_system.link_ports("PostoDeEnfermagem", "AlaDeMedicacao", "AlaDeMedicacao", "PostoDeEnfermagem");
+
+        //Configura o rastreio para o simulador (default, entity, event)
+        Sim_system.set_trace_detail(false, true, false);
+
+        Sim_system.run();
     }
 
-}
-
-class SimEntity extends Sim_entity {
-
-  public SimEntity(String s) {
-    super(s);
-  }
-
-  public SimEntity(String s, String s1, int i, int i1) {
-    super(s, s1, i, i1);
-  }
 }
